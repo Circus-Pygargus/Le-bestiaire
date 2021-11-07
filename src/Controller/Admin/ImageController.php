381ad9134bf1c6b39cda8ca4e293caa0f4b69889
Circus-Pygargus <richard.meuret@laposte.net>
2021-11-07 22:33:31 +0100

    [FIX] simplification de la gestion du formulaire de création d'image

--- src/Controller/Admin/ImageController.php
+++ src/Controller/Admin/ImageController.php
@@ -23,12 +23,6 @@ class ImageController extends AdminController
         $image = new Image();
         $this->denyAccessUnlessGranted('IMAGE_CREATE', $image);
 
-        return $this->render('admin/image/create.html.twig');
-    }
-
-    public function new (Request $request): Response
-    {
-        $image = new Image();
         $form = $this->createForm(CreateImageFormType::class, $image);
 
         $form->handleRequest($request);
@@ -40,7 +34,7 @@ class ImageController extends AdminController
             $em->flush();
         }
 
-        return $this->renderForm('admin/image/layouts/new.html.twig', [
+        return $this->renderForm('admin/image/create.html.twig', [
             'imageForm' => $form
         ]);
     }
