025cb59b4ae22fb323d171a0ddab7d331eab5e4c
Circus-Pygargus <richard.meuret@laposte.net>
2021-11-07 12:11:54 +0100

    [MONSTER] ajout affichage et gestion du formulaire de création de monster

--- src/Controller/Admin/MonsterController.php
+++ src/Controller/Admin/MonsterController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Monster;
+use App\Form\CreateMonsterFormType;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,13 +17,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class MonsterController extends AdminController
 {
     /**
-     * @Route("/create", name="create")
+     * @Route("/create/{categorySlug}", name="create")
      */
-    public function create (): Response
+    public function create (Request $request, string $categorySlug, CategoryRepository $categoryRepository): Response
     {
         $monster = new Monster();
         $this->denyAccessUnlessGranted('MONSTER_CREATE', $monster);
 
-        return $this->render('admin/monster/create.html.twig');
+        $category = $categoryRepository->findOneBy(['slug' => $categorySlug]);
+        $monster->setCategory($category);
+
+        $form = $this->createForm(CreateMonsterFormType::class, $monster);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($monster);
+            $em->flush();
+        }
+
+        return $this->renderForm('admin/monster/create.html.twig', [
+            'monsterForm' => $form
+        ]);
     }
 }
