e08ba7189514beea359a00fed738072fcdd9e7da
Circus-Pygargus <richard.meuret@laposte.net>
2021-11-07 17:43:16 +0100

    [WYSIWYG] dans le Heading : uniquement p et h3

--- assets/js/ckeditor-manager.js
+++ assets/js/ckeditor-manager.js
@@ -1,7 +1,21 @@
 const ckEditorManager = () => {
     ClassicEditor
         // Initialisation ckeditor
-        .create(document.querySelector('#editor'))
+        .create(document.querySelector('#editor'), {
+            heading: {
+                options: [{
+                    model: 'paragraph',
+                    title: 'Paragraphe',
+                    class: 'ck-heading-paragraph'
+                },
+                {
+                    model: 'heading3',
+                    title: 'Propriété',
+                    class: 'ck-heading-heading3',
+                    view: 'h3'
+                }]
+            }
+        })
         .then(editor => {
               editor.sourceElement.parentElement.addEventListener('submit', (e) => {
                   e.preventDefault();
